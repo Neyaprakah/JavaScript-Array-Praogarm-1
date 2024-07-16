90f86cb39a0917ae0b45116e5607d3eedbd928fe
Neyaprakash <141732433+Neyaprakah@users.noreply.github.com>
2024-07-16 17:04:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+//Program 1
+//Array Creation
+
+// let arr=["Lion","Tiger","Wolf"];
+// console.log("Score=",arr);
+
+
+
+//Program 2
+//Array Manuplation
+
+// let arr=["Lion","Tiger","Wolf"];
+// arr.push("Elephant")
+// console.log(arr);
+// arr.pop("Elephant")
+// console.log(arr);
+// arr.unshift("Monkey")
+// console.log(arr);
+// arr.shift("Monkey");
+// console.log(arr);
+
+
+
+//Program 3
+//Array Searching
+
+// let arr=["Lion","Tiger","Wolf"];
+// console.log(arr.indexOf("Tiger"));
+// console.log(arr.includes("Wolf"));
+
+
+
+//Program 4
+// Array Filtering
+
+// let arr=[1,2,3,4,5,6,7,8];
+// let arr2=[];
+// let j=0;
+// for(let i=0;i<arr.length;i++)
+// {
+//     if(arr[i]%2==0)
+//     {
+//         arr2[j]=arr[i];
+//         j=j+1;
+//     }
+// }
+// console.log(arr);
+// console.log(arr2);
+
+
+
+//Program 5
+//Array Mapping
+
+// let arr=[1,2,3,4,5,6,7,8,9]
+// let arr2=arr.map(x=>x*x);
+// console.log(arr);
+// console.log(arr2);
